79fc36846be8d9122e55642ca1baa98bed497c71
Evgeny Karasev <gnomskg@gmail.com>
2017-08-31 18:03:32 +0300

    Added some test for testing returning instance of ModuleFragment by calling createModuleFragment()

--- src/app/modules/utils/__tests__/createModuleFragment.js
+++ src/app/modules/utils/__tests__/createModuleFragment.js
@@ -1,23 +1,65 @@
 import createModuleFragment from '../createModuleFragment';
 import ModuleFragment from '../ModuleFragment';
 
-describe('createModuleFragment(config)', () => {
-  const correctConfig = {
-    type: 'reducer',
-    body: {},
-  };
-  const invalidConfig = {
-    body: {},
-  };
+const CORRECT_TYPE = 'reducer';
+const correctConfig = {
+  type: CORRECT_TYPE,
+  body: {},
+};
 
+const invalidConfig = {
+  body: {},
+};
+
+const REDUCER_TYPE = 'reducer';
+const reducerConfig = {
+  type: REDUCER_TYPE,
+  body: {},
+};
+
+const COMPONENT_TYPE = 'component';
+const componentConfig = {
+  type: COMPONENT_TYPE,
+  body: {},
+};
+
+const SAGA_TYPE = 'saga';
+const sagaConfig = {
+  type: SAGA_TYPE,
+  body: {},
+};
+
+describe('createModuleFragment(config)', () => {
   it('should return instance of ModuleFragment', () => {
     const moduleFragment = createModuleFragment(correctConfig);
 
     expect(moduleFragment).toBeInstanceOf(ModuleFragment);
   });
-  it('should throw TypeError if config invalid', () => {
+  it('should throw TypeError if can not create fragment with getting config', () => {
     const createModuleFragmentCaller = () => createModuleFragment(invalidConfig);
 
     expect(createModuleFragmentCaller).toThrow(TypeError);
+  });
+  // Проверять сообщения ошибок, могут неправильно конфигурироваться
+  it('created ModuleFragment should return pare with "reducer" type at first position if get config for reducer', () => {
+    const moduleFragment = createModuleFragment(reducerConfig);
+
+    const [gettingType] = moduleFragment.get();
+
+    expect(gettingType).toEqual(REDUCER_TYPE);
+  });
+  it('created ModuleFragment should return pare with "component" type at first position if get config for component', () => {
+    const moduleFragment = createModuleFragment(componentConfig);
+
+    const [gettingType] = moduleFragment.get();
+
+    expect(gettingType).toEqual(COMPONENT_TYPE);
+  });
+  it('created ModuleFragment should return pare with "saga" type at first position if get config for saga', () => {
+    const moduleFragment = createModuleFragment(sagaConfig);
+
+    const [gettingType] = moduleFragment.get();
+
+    expect(gettingType).toEqual(SAGA_TYPE);
   });
 });

67d9b66edd15c35b676ff0d212f806c801be393e
Evgeny Karasev <gnomskg@gmail.com>
2017-08-31 18:02:26 +0300

    Fixed message for TypeError in ModuleFragments constructor()

--- src/app/modules/utils/ModuleFragment.js
+++ src/app/modules/utils/ModuleFragment.js
@@ -1,7 +1,7 @@
 export default class ModuleFragment {
   constructor(pattern) {
     if (typeof pattern !== 'function') {
-      throw new TypeError('ModuleFragment => constructor(): getting pattern must be function');
+      throw new TypeError('ModuleFragment => constructor(pattern): getting pattern must be function');
     }
 
     this.get = () => pattern();

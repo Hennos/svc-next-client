324416430c9f79ea0cc114781a5c2d378c9f9903
Evgeny Karasev <gnomskg@gmail.com>
2017-10-06 14:39:16 +0300

    outdated comment

--- src/app/modules/conference/index.js
+++ src/app/modules/conference/index.js
@@ -1,7 +1,6 @@
 import configurateModule from '../utils/configurateModule';
 import component from './component';
 
-// todo: очень много писанины, необходимо по максимуму ужать до лаконичного api
 export default configurateModule([{
   type: 'component',
   body: component,

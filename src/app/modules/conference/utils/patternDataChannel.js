ac64d5b20c6960bb57a8b6a07f1805f3961955ba
Evgeny Karasev <gnomskg@gmail.com>
2017-10-29 04:41:22 +0300

    add pattern for creating peer-to-peer channel

---
+++ src/app/modules/conference/utils/patternDataChannel.js
@@ -0,0 +1,13 @@
+import P2PConnecter from './P2PConnecter';
+import Channel from './Channel';
+
+export default async function patternDataChannel(connecter) {
+  const argIsConnecter = connecter instanceof P2PConnecter;
+  if (!argIsConnecter) {
+    throw new TypeError('patternDataChannel: connecter is not an instance of P2PConnecter');
+  }
+
+  const connection = await connecter.createDataChannel();
+
+  return Channel.create(connection);
+}

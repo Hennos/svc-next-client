ff4216f37443044d73b1718c482c94c3aa96fd76
Evgeny Karasev <gnomskg@gmail.com>
2017-11-03 15:50:50 +0300

    rewrite P2PChannel Api to synchronous

--- src/app/modules/conference/utils/P2PChannel.js
+++ src/app/modules/conference/utils/P2PChannel.js
@@ -1,29 +1,15 @@
 import Channel from './Channel';
-import P2PConnecter from './P2PConnecter';
 
 export default class P2PChannel extends Channel {
-  static create(connecter) {
-    return new P2PChannel(connecter);
+  static create(connection) {
+    return new P2PChannel(connection);
   }
 
-  constructor(connecter) {
-    const argIsConnecter = connecter instanceof P2PConnecter;
-    if (!argIsConnecter) {
-      throw new TypeError('P2PChannel => constructor(connecter): dataChannel is not an instance of P2PConnecter');
-    }
-
-    connecter.createDataChannel()
-    .then((channel) => {
-      super({
-        send: channel.send.bind(channel),
-        onmessage: channel.onmessage.bind(channel),
-        close: channel.close.bind(channel),
-      });
-
-      const cbIsFunc = typeof this.onopen === 'function';
-      if (cbIsFunc) {
-        this.onopen.call(this);
-      }
+  constructor(connection) {
+    super({
+      send: connection.send.bind(connection),
+      onmessage: connection.onmessage.bind(connection),
+      close: connection.close.bind(connection),
     });
   }
 }

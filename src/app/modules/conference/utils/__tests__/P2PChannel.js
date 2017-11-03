c261183fe8f58e2a33e6e7635907bea16bed9029
Evgeny Karasev <gnomskg@gmail.com>
2017-11-03 03:02:22 +0300

    delete useless test suite

--- src/app/modules/conference/utils/__tests__/P2PChannel.js
+++
@@ -1,7 +0,0 @@
-import P2PChannel from '../P2PChannel';
-import IOChannel from '../IOChannel';
-import Channel from '../Channel';
-
-describe('P2PChannel', () => {
-
-});
--- src/app/modules/conference/utils/__tests__/P2Pconnecter.js
+++
@@ -1,6 +0,0 @@
-import P2PConnecter from '../P2PConnecter';
-import IOChannel from '../IOChannel';
-
-describe('P2PConnecter', () => {
-
-});

fbed707422a764ab2c1418119cdf23aa59997eb3
Evgeny Karasev <gnomskg@gmail.com>
2017-11-03 03:03:47 +0300

    add component for displaying connected peers

--- src/app/modules/conference/components/Users.jsx
+++ src/app/modules/conference/components/Users.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { stateKeys } from '../constants';
+import {
+  connectPeer as connectWithUser,
+} from '../actions';
 
-function Users({ users }) {
+function Users({ users, createConnection }) {
   return (
     <div>{
-      users.map(user => (
-        <div key={user}>{user.name}</div>
+      Object.keys(users).map(id => (
+        <button onClick={() => createConnection(id)} key={id}>{users[id].name}</button>
       ))
     }</div>
   );
 }
 
 const mapStateToProps = state => ({
-  users: state.conference.get(stateKeys.users).toArray(),
+  users: state.conference.get(stateKeys.users).toObject(),
 });
 
-export default connect(mapStateToProps)(Users);
+const mapDispatchToProps = dispatch => ({
+  createConnection: user => dispatch(connectWithUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Users);

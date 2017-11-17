854f52f1f3899756b2ba977695f6aa77c18c8362
Evgeny Karasev <gnomskg@gmail.com>
2017-11-17 19:58:28 +0300

    add base style for  active call

---
+++ src/app/modules/conference/components/Connected/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './style.css';
+
+function Connected({ className, desc }) {
+  return (
+    <div className={classNames(className, 'connected')}>
+      <video>
+        <source src="" />
+      </video>
+    </div>
+  );
+}
+
+Connected.propTypes = {
+  className: PropTypes.string,
+  desc: PropTypes.shape({
+    name: PropTypes.string,
+  }).isRequired,
+};
+
+Connected.defaultProps = {
+  className: '',
+};
+
+export default Connected;
